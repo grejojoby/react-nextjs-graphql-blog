05a02b0385ff8fb7d63e576e03bc0a1b3495cb73
Grejo Joby <grejo00@gmail.com>
2021-11-16 20:17:04 +0530

    homepage

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import Head from 'next/head';
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'Nextjs Testing', excerpt: 'Learn Next JS' },
-  { title: 'Tailwind Testing', excerpt: 'Learn Tailwing' },
-]
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -16,7 +12,7 @@ export default function Home() {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (<PostCard post={post} key={post.title} />))}
+          {posts.map((post, index) => (<PostCard post={post.node} key={post.node.title} />))}
         </div>
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
@@ -31,4 +27,12 @@ export default function Home() {
 
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
